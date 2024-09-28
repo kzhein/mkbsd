9adec14802cb6185710c9c72c72b86808e17da34
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2024-09-28 15:48:21 +0630

    Add search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 interface Props {
   wallpapers: Wallpaper[];
@@ -12,10 +13,14 @@ interface Wallpaper {
   preview: string;
   sd: string;
   hd: string;
+  searchTerms: string[];
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const [meta, wallpapersUrls] = await Promise.all([
+  const [metadata, content, media] = await Promise.all([
+    fetch(
+      'https://storage.googleapis.com/panels-api/data/20240916/content-metadata-1a'
+    ).then(res => res.json()),
     fetch(
       'https://storage.googleapis.com/panels-api/data/20240916/content-1a'
     ).then(res => res.json()),
@@ -24,14 +29,20 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     ).then(res => res.json()),
   ]);
 
-  const wallpapers: Wallpaper[] = meta.wallpapers.map((wp: any) => ({
+  const wallpapers: Wallpaper[] = content.wallpapers.map((wp: any) => ({
     label: wp.label,
     blurHash: wp.previews.standard[0].blurHash,
     preview:
-      wallpapersUrls.data[wp.previews.standard[0].id].wfs ||
-      wallpapersUrls.data[wp.previews.standard[0].id].s,
-    sd: wallpapersUrls.data[wp.dlm.sd].dsd,
-    hd: wallpapersUrls.data[wp.dlm.hd].dhd,
+      media.data[wp.previews.standard[0].id].wfs ||
+      media.data[wp.previews.standard[0].id].s,
+    sd: media.data[wp.dlm.sd].dsd,
+    hd: media.data[wp.dlm.hd].dhd,
+    searchTerms: [
+      wp.label.toLowerCase(),
+      ...metadata.remixMetadata
+        .find((mt: any) => mt.remixId === wp.id)
+        .searchTerms.map((st: any) => st.t),
+    ],
   }));
 
   return {
@@ -41,6 +52,19 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 };
 
 export default function Home({ wallpapers }: Props) {
+  const router = useRouter();
+  const search = router.query?.search || '';
+
+  let wallpapersToShow: Wallpaper[];
+
+  if (!!search) {
+    wallpapersToShow = wallpapers.filter(wp =>
+      wp.searchTerms.some(st => st.includes((search as string).toLowerCase()))
+    );
+  } else {
+    wallpapersToShow = wallpapers;
+  }
+
   return (
     <div>
       <Head>
@@ -49,25 +73,47 @@ export default function Home({ wallpapers }: Props) {
 
       <h1 className='text-center text-6xl font-bold my-3'>MKBSD</h1>
 
-      <div className='container mx-auto px-4 pb-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {wallpapers.map(wp => (
-          <a href={wp.hd} target='_blank' key={wp.hd}>
-            <div className='cursor-pointer'>
-              <Image
-                unoptimized
-                placeholder='empty'
-                className='h-[500px] w-full object-cover'
-                width={500}
-                height={500}
-                src={wp.sd}
-                alt={wp.label}
-              />
-              <div className='p-2'>
-                <p className='text-lg'>{wp.label}</p>
+      <div className='container mx-auto px-4 pb-4'>
+        <div className='my-4'>
+          <input
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-1/2 mx-auto p-2.5'
+            placeholder='Search Wallpapers'
+            type='text'
+            value={search}
+            onChange={e => {
+              const url = new URL(window.location.href);
+              url.searchParams.set('search', e.target.value);
+              router.replace(url, undefined, { shallow: true });
+            }}
+          />
+          {!!search && (
+            <p className='my-2'>
+              <span className='font-bold'>{wallpapersToShow.length}</span>{' '}
+              wallpapers found
+            </p>
+          )}
+        </div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {wallpapersToShow.map(wp => (
+            <a href={wp.hd} target='_blank' key={wp.hd}>
+              <div className='cursor-pointer'>
+                <Image
+                  unoptimized
+                  placeholder='empty'
+                  className='h-[500px] w-full object-cover'
+                  width={500}
+                  height={500}
+                  src={wp.sd}
+                  alt={wp.label}
+                />
+                <div className='p-2'>
+                  <p className='text-lg'>{wp.label}</p>
+                </div>
               </div>
-            </div>
-          </a>
-        ))}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
